ad3a59e5ea1dcf901cf2bf767d08af1dccbb891f
JFAldridge <jfaldridge1985@gmail.com>
2020-01-09 21:44:24 -0500

    Dries up session_helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,6 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def logged_in?
@@ -14,18 +11,6 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
   end
 
   def current_user
@@ -43,6 +28,18 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end  
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
